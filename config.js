b266d399b4bb933ac5115797f5b833a2b45c1f47
JEREMIKURNIAWAN <102877498+JEREMIKURNIAWAN@users.noreply.github.com>
2022-06-27 03:43:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6285822347348'] // Put your number here
+global.owner = ['6281315252010'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -44,7 +44,7 @@ global.APIKeys = { // APIKey Here
 global.stiker_wait = '*Stiker sedang dibuat*'
 global.packname = 'stickerby'
 global.author = `ArullOfc`
-global.wm = 'ArullBotzMD'
+global.wm = 'JER-BotzMD'
 global.watermark = wm
 global.media = 'https://telegra.ph/file/6129a084ac3e0986bf31c.jpg'
 global.instagram = 'https://instagram.com/sahrulwara_____'
